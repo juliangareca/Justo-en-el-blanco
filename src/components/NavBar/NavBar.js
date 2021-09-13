283ddc35ee3bf56d079886e5501218d14e625c9d
Julian Gareca Kandrachoff <jugakan@gmail.com>
2021-09-13 12:56:47 -0300

    Clase 3 - NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,20 +2,20 @@ const NavBar = () =>{
 
     return  <nav className="navbar navbar-expand-lg navbar-light bg-light">
     <div className="container-fluid">
-      <a className="navbar-brand" href="#">Justo en el Blanco</a>
+      <a className="navbar-brand" href="../index.html">Justo en el Blanco</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNavDropdown">
           <ul className="navbar-nav">
           <li className="nav-item">
-            <a className="nav-link" href="#">Sabanas</a>
+            <a className="nav-link" href="../sabanas.html">Sabanas</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Frazadas</a>
+            <a className="nav-link" href="../frazadas.html">Frazadas</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Toallas</a>
+            <a className="nav-link" href="../toallas.html">Toallas</a>
           </li>
         </ul>
       </div>
